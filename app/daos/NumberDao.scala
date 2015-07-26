4393d1069363f35a7cce119237613d6aae4055f5
Bedrytski Aliaksandr <meanor89@gmail.com>
2015-07-26 12:28:57 +0200

    Quick fix

--- app/daos/NumberDao.scala
+++ app/daos/NumberDao.scala
@@ -1,5 +1,6 @@
 package daos
 
+import play.api.Play.current
 import play.api.db.DB
 
 class NumberDao {
@@ -7,13 +8,13 @@ class NumberDao {
   def generateNumbers(): List[Int] = {
     val conn = DB.getConnection()
 
-    var resultList = Nil
+    var resultList = List.empty[Int]
     try {
       val stmt = conn.createStatement
       val rs = stmt.executeQuery("SELECT numb from test_table")
 
       while (rs.next()) {
-        resultList += rs.getString("numb")
+        resultList = resultList :+ rs.getInt("numb")
       }
     } finally {
       conn.close()
